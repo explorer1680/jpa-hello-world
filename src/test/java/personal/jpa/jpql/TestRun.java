a532f44c2d2bdd2159fa9c71a08d18e079495874
Ethan Cheng <Ethan@jinsongs-air>
2017-06-03 18:54:09 -0400

    update some files.

--- src/test/java/personal/jpa/jpql/TestRun.java
+++ src/test/java/personal/jpa/jpql/TestRun.java
@@ -1,13 +1,25 @@
 package personal.jpa.jpql;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class TestRun {
     public static void main(String[] args){
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpql");
-
-
+        EntityManager manager = factory.createEntityManager();
+        EntityTransaction ts = manager.getTransaction();
+        ts.begin();
+        
+        
+        
+        
+        
+        
+        
+        ts.commit();
+        manager.close();
 
         factory.close();
     }
