acfd874845c7e820d32093e832f4429d86158150
echeng <cjs1234567@hotmail.com>
2017-11-03 14:13:37 -0400

    update comment to note.txt for jpql.basic.

--- src/main/java/personal/jpa/Employee.java
+++ src/main/java/personal/jpa/Employee.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue//(strategy = GenerationType.IDENTITY)
     private Integer eid;
 
     private String ename;
